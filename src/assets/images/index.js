ecc13dc6c48e56295505416dbebaba13686e2ab8
Aryan Nandanwar <116783328+AryanNandanwar@users.noreply.github.com>
2023-09-26 00:07:45 +0530

    Uploaded SRC

---
+++ src/assets/images/index.js
@@ -0,0 +1,67 @@
+import logo from "./orebiLogo.png";
+import logoLight from "./logoLight.png";
+import bannerImgOne from "./banner/bannerImgOne.webp";
+import bannerImgTwo from "./banner/bannerImgTwo.webp";
+import bannerImgThree from "./banner/bannerImgThree.webp";
+import saleImgOne from "./sale/saleImgOne.webp";
+import saleImgTwo from "./sale/saleImgTwo.webp";
+import saleImgThree from "./sale/saleImgThree.webp";
+// ============== Products Start here ====================
+// New Arrivals
+import newArrOne from "./products/newArrival/newArrOne.webp";
+import newArrTwo from "./products/newArrival/newArrTwo.webp";
+import newArrThree from "./products/newArrival/newArrThree.webp";
+import newArrFour from "./products/newArrival/newArrFour.webp";
+
+// Best Sellers
+import bestSellerOne from "./products/bestSeller/bestSellerOne.webp";
+import bestSellerTwo from "./products/bestSeller/bestSellerTwo.webp";
+import bestSellerThree from "./products/bestSeller/bestSellerThree.webp";
+import bestSellerFour from "./products/bestSeller/bestSellerFour.webp";
+
+// Special Offers
+
+import Virginia from "./products/specialOffer/Virginianew.jpg"
+import Bratz from "./products/specialOffer/Bratznew.jpg"
+import Fruits from "./products/specialOffer/Fruitsnew.jpg"
+import UGH from "./products/specialOffer/UGHnew.jpg"
+
+// Year Product
+import productOfTheYear from "./products/productOfTheYear.webp";
+// ============== Products End here ======================
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  // New Arrivals
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+
+  // Best Sellers
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+
+  // Sprcial Offers
+  Virginia,
+  UGH,
+  Fruits,
+  Bratz,
+
+  // Year Product
+  productOfTheYear,
+  // ===================== Products End here ==============
+  paymentCard,
+  emptyCart,
+};
---
+++ src/components/home/SpecialOffers/SpecialOffers.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Heading from "../Products/Heading";
+import Product from "../Products/Product";
+import {
+  
+  Virginia,
+  UGH,
+  Fruits,
+  Bratz,
+
+  
+} from "../../../assets/images/index";
+
+const SpecialOffers = () => {
+  return (
+    <div className="w-full pb-20">
+      <Heading heading="Special Offers" />
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 xl:grid-cols-4 gap-10">
+        <Product
+          _id="1101"
+          img={Virginia}
+          productName="Virginia Wolf Quote"
+          price="300"
+          color="Black"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="1102"
+          img={UGH}
+          productName="UGH Text"
+          price="250"
+          color="Black"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="1103"
+          img={Fruits}
+          productName="Fruits Design"
+          price="250"
+          color="White"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="1104"
+          img={Bratz}
+          productName="Bratz Text"
+          price="325"
+          color="Red"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SpecialOffers;
